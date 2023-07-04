5e03889b0ea00febe978d91d1549ce2db77d8b67
Deepti Karambelkar <deepti.karambelkar2016@gmail.com>
2023-07-03 18:34:30 -0700

    updates - 3rd july

--- EmployeeMgmt/wwwroot/js/site.js
+++ EmployeeMgmt/wwwroot/js/site.js
@@ -2,3 +2,48 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+$(function () {
+    $.ajaxSetup({ cache: false });
+
+//$("a").on("click", function (e) {
+    
+//    var url = this.href;
+//    $('#').load(url, function () {
+//        $('#').modal('show');
+//        onSubmitOfModal(this);
+//    });
+
+//    return false;
+//});
+
+function onSubmitOfModal(dialog) {
+    $('form', dialog).submit(function () {
+
+        var action = this.action;
+        var method = this.method;
+        var formSubmissionData = $(this).serialize();
+
+        $.ajax({
+            url: action,
+            type: method,
+            data: formSubmissionData,
+            success: function (result) {
+                if (result.success) {
+                    $('#myModal').modal('hide');
+                    location.reload();
+                } else {
+                    $('#myModalContent').html(result);
+                    onSubmitOfModal();
+                }
+            }
+        });
+        return false;
+    });
+}
+
+
+
+
+});
+
